538f2d5e2f68aca6602076551c4bb638bb63ff73
Андрей <mitzyk@mail.ru>
2015-04-26 02:01:26 +0300

    Доработана генерация отчетов Excel Исправлено неверное распознавание формата данных Добавлено автоматическое установление размера столбцов по ширине содержимого Добавлено сообщение об ошибке, при попытке генерации отчета с незагруженными данными

--- ViewRidgeAssistant/VRA.BusinessLayer/ReportGenerator.cs
+++ ViewRidgeAssistant/VRA.BusinessLayer/ReportGenerator.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
+using System.Threading;
+using System.Windows;
 using VRA.Dto;
 using Excel = Microsoft.Office.Interop.Excel;
 
@@ -11,110 +14,122 @@ namespace VRA.BusinessLayer
 
         public void fillExcelTableByType(IEnumerable<object> grid, string status)
         {
-            Excel.Application ExcelApp = new Excel.Application();
-            Excel.Workbook ExcelWorkBook = ExcelApp.Workbooks.Add(System.Reflection.Missing.Value);
-            Excel.Worksheet excel = (Excel.Worksheet)ExcelWorkBook.Worksheets.Item[1];
+            if (grid != null)
+            {
+                Excel.Application ExcelApp = new Excel.Application();
+                Excel.Workbook ExcelWorkBook = ExcelApp.Workbooks.Add(System.Reflection.Missing.Value);
+                Excel.Worksheet excel = (Excel.Worksheet) ExcelWorkBook.Worksheets.Item[1];
 
+                int x = 1;
+                int y = 1;
 
-            int x = 1;
-            int y = 1;
+                // Устанавливает фиксированный десятичный разделитель (нужно для верного распознавания типа данных Excel'ем) 
+                CultureInfo cultureInfo = new CultureInfo(Thread.CurrentThread.CurrentCulture.Name);
+                Thread.CurrentThread.CurrentCulture = cultureInfo;
+                cultureInfo.NumberFormat.NumberDecimalSeparator = ".";
 
-            // первая строка (шапка таблицы) -- жирным стилем
-            excel.Cells[y, 1].EntireRow.Font.Bold = true;
+                // первая строка (шапка таблицы) -- жирным стилем
+                excel.Cells[y, 1].EntireRow.Font.Bold = true;
 
-            // вырванивание ткста в ячейках -- по левому краю
-            excel.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignLeft;
+                // вырванивание ткста в ячейках -- по левому краю
+                excel.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignLeft;
 
-            // формат ячеек -- текстовой
-            excel.Cells.NumberFormat = "@";
+                // Устанавливает формат ячеек
+                excel.Cells.NumberFormat = "General";
 
-            /* Пример с перебором Properties объекта. Данный пример демонстрирует, как можно
+                /* Пример с перебором Properties объекта. Данный пример демонстрирует, как можно
              * перебирая объявленные в данном классе property, записывать их в Excel. 
              * При этом, последовательность полей соответствует последовательности их
              * объявления в соответствующем используемом классе.
              * Данный вариант удобен автоматическим добавлением новых / удалением полей в итоговой таблице 
              * при изменении целевого класса. */
 
-            Object dtObj = new Object(); // пустой объект для получения списка property
+                Object dtObj = new Object(); // пустой объект для получения списка property
 
-            switch (status)
-            {
-                case "Customer":
-                    dtObj = new CustomerDto();
-                    break;
-                case "Artist":
-                    dtObj = new ArtistDto();
-                    break;
-                case "Work":
-                    dtObj = new WorkDto();
-                    break;
-                case "Trans":
-                    dtObj = new TransactionDto();
-                    break;
-                case "Interests":
-                    dtObj = new InterestsDto();
-                    break;
-                case "Nations":
-                    dtObj = new NationDto();
-                    break;
-            }
-
-            // генерация шапки таблицы
-            foreach (var prop in dtObj.GetType().GetProperties())
-            {
-                excel.Cells[y, x] = prop.Name.Trim();
-                x++;
-            }
-
-            // генерация строк-записей таблицы
-            foreach (var item in grid)
-            {
-                y++;
-
-                Object itemObj = item; // объект-контейнер для текущего читаемого элемента
-
-                x = 1;
-                foreach (var prop in itemObj.GetType().GetProperties())
+                switch (status)
                 {
-                    object t = prop.GetValue(itemObj, null);
+                    case "Customer":
+                        dtObj = new CustomerDto();
+                        break;
+                    case "Artist":
+                        dtObj = new ArtistDto();
+                        break;
+                    case "Work":
+                        dtObj = new WorkDto();
+                        break;
+                    case "Trans":
+                        dtObj = new TransactionDto();
+                        break;
+                    case "Interests":
+                        dtObj = new InterestsDto();
+                        break;
+                    case "Nations":
+                        dtObj = new NationDto();
+                        break;
+                }
 
-                    String val;
-
-                    if (t == null)
-                        val = "";
-                    else
-                    {
-                        val = t.ToString();
-                        // Если тип сложный, то вытаскиваем нужное поле.
-                        if (t is NationDto)
-                            val = ((NationDto)t).Nationality;
-                        if (t is ArtistDto)
-                            val = ((ArtistDto)t).Name;
-                        if (t is CustomerDto)
-                            val = ((CustomerDto)t).Name;
-                        if (t is WorkDto)
-                            val = ((WorkDto)t).Title;
-                    }
-                    excel.Cells[y, x] = val.Trim();
+                // генерация шапки таблицы
+                foreach (var prop in dtObj.GetType().GetProperties())
+                {
+                    excel.Cells[y, x] = prop.Name.Trim();
                     x++;
                 }
+
+                // генерация строк-записей таблицы
+                foreach (var item in grid)
+                {
+                    y++;
+
+                    Object itemObj = item; // объект-контейнер для текущего читаемого элемента
+
+                    x = 1;
+                    foreach (var prop in itemObj.GetType().GetProperties())
+                    {
+                        object t = prop.GetValue(itemObj, null);
+
+                        String val;
+
+                        if (t == null)
+                            val = "";
+                        else
+                        {
+                            val = t.ToString();
+                            // Если тип сложный, то вытаскиваем нужное поле.
+                            if (t is NationDto)
+                                val = ((NationDto) t).Nationality;
+                            if (t is ArtistDto)
+                                val = ((ArtistDto) t).Name;
+                            if (t is CustomerDto)
+                                val = ((CustomerDto) t).Name;
+                            if (t is WorkDto)
+                                val = ((WorkDto) t).Title;
+                        }
+                        excel.Cells[y, x] = val.Trim();
+                        x++;
+                    }
+                }
+                // Устанавливаем размер колонок по ширине содержимого.
+                excel.Columns.AutoFit();
+                ExcelApp.Visible = true;
             }
-            ExcelApp.Visible = true;
+            else MessageBox.Show("Данные не загружены!");
         }
+
 
         public string genHtmlWorksInGallery(string rep)
         {
             List<object> works = ProcessFactory.GetWorkProcess().GetListInGallery().Cast<object>().ToList();
             List<object> trans = ProcessFactory.GetTransactionProcess().GetListInGallery().Cast<object>().ToList();
 
-            string res_html = "<tr><td><b>Код</b></td><td><b>Название</b></td><td><b>Художник</b></td><td><b>Цена</b></td><td><b>Описание</b></td></tr>";
+            string res_html =
+                "<tr><td><b>Код</b></td><td><b>Название</b></td><td><b>Художник</b></td><td><b>Цена</b></td><td><b>Описание</b></td></tr>";
 
             foreach (var w_item in works)
             {
-                WorkDto WorkItem = (WorkDto)w_item;
+                WorkDto WorkItem = (WorkDto) w_item;
                 foreach (var t_item in trans)
                 {
-                    TransactionDto TransItem = (TransactionDto)t_item;
+                    TransactionDto TransItem = (TransactionDto) t_item;
                     if ((WorkItem.Id == TransItem.Work.Id) && (TransItem.Customer == null))
                     {
                         ArtistDto ArtistItem = ProcessFactory.GetArtistProcess().Get(WorkItem.Artist.Id);
